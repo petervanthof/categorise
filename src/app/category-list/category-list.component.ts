50ea9f466729dac2f513300f151c1d3b6f4a7a20
Peter van 't Hof <peter@petervanthof.it>
2018-05-08 21:06:02 +0200

    Adding an item to an existing category

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -26,30 +26,55 @@ export class CategoryListComponent {
   constructor() {
     this.categories = [
       this.createCategory('Artist', [
-        this.createCategory('Virtual Self', [], ['Ghost Voices']),
+        this.createCategory('VirtualSelf', [], ['Ghost Voices']),
         this.createCategory('Huem', [], ['Your Smile (Original Mix)']),
-        this.createCategory('Gabriel & Dresden', [], ['Arcadia', 'Only Road (Cosmic Gate Extended Mix) (feat. Sub Teal)'])], []),
+        this.createCategory('GabrielDresden', [], ['Arcadia', 'Only Road (Cosmic Gate Extended Mix) (feat. Sub Teal)'])], []),
       this.createCategory('Genre', [
         this.createCategory('Trance', [], ['Your Smile (Original Mix)']),
-        this.createCategory('Dance/Electro', [], ['Ghost Voices', 'Arcadia', 'Only Road (Cosmic Gate Extended Mix) (feat. Sub Teal)'])], []),
+        this.createCategory('DanceElectro', [], ['Ghost Voices', 'Arcadia', 'Only Road (Cosmic Gate Extended Mix) (feat. Sub Teal)'])], []),
       this.createCategory('Year', [
         this.createCategory('2018', [], ['Your Smile (Original Mix)', 'Only Road (Cosmic Gate Extended Mix) (feat. Sub Teal)']),
         this.createCategory('2005', [], ['Arcadia']),
         this.createCategory('2017', [], ['Ghost Voices'])], [])];
   }
 
-  private createCategory(name: string, children: Category[], items: string[]): Category {
-    return new Category(this.counter++, name, children, items);
-  }
+  addCategories(categories: HTMLInputElement, item: HTMLInputElement): boolean {
+    categories.value.split(' ').forEach(category => this.findOrCreateCategory(category).addItem(item.value));
 
-  addCategory(category: HTMLInputElement, item: HTMLInputElement): boolean {
-    this.categories.push(new Category(this.counter++, category.value, [], [item.value]));
     this.tree.treeModel.update();
 
-    category.value = '';
+    categories.value = '';
     item.value = '';
 
     return false;
+  }
+
+  private findOrCreateCategory(name: string): Category {
+    let category = this.findCategory(name);
+
+    if (!category) {
+      category = this.createCategory(name, [], []);
+
+      this.categories.push(category);
+    }
+
+    return category;
+  }
+
+  private findCategory(name: string): Category {
+    for (let i = 0; i < this.categories.length; i++) {
+      const category = this.categories[i].findCategory(name);
+
+      if (category) {
+        return category;
+      }
+    }
+
+    return null;
+  }
+
+  private createCategory(name: string, children: Category[], items: string[]): Category {
+    return new Category(this.counter++, name, children, items);
   }
 
   setSelectedCategory(event): boolean {
--- src/app/category.model.ts
+++ src/app/category.model.ts
@@ -3,4 +3,27 @@ export class Category {
   constructor(public id: number, public name: String, public children: Category[], public items: string[]) {
   }
 
+  addItem(item: string) {
+    if (item) {
+      this.items.push(item);
+    }
+  }
+
+  findCategory(name: string): Category {
+    if (this.name === name) {
+      return this;
+    }
+
+    for (let i = 0; i < this.children.length; i++) {
+      const child = this.children[i];
+      const category = child.findCategory(name);
+
+      if (category) {
+        return category;
+      }
+    }
+
+    return null;
+  }
+
 }
